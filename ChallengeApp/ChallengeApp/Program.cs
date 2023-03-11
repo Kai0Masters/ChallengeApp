ff20ee7c99588ac1315c696e2c19a37dc37cbacc
Kai Masters <adrianflor@wp.pl>
2023-03-11 13:07:46 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4
+
+// name przykłady = Anna, Ewa, Iza, Wioleta, Adam, Piotr, Maciej, Wiktor
+
+string name = "Ewa";
+string name2 = "Piotr";
+
+// wiek osób (0-100)
+
+var age = 24;
+var age2 = 33;
+var age3 = 15;
+
+// pierwsza część zadania
+
+if (name.EndsWith("a"))
+{
+    Console.WriteLine("Kobieta");
+}
+else
+{
+    Console.WriteLine("Mężczyzna");
+}
+
+if (age < 30)
+{
+    Console.WriteLine("poniżej 30 lat");
+}
+
+// druga część zadania
+
+Console.WriteLine(name + ", lat " + age2);
+
+// trzecia część zadania
+
+if (age3 < 18)
+{
+    Console.WriteLine("Niepełnoletni");
+}
+
+if (name2.EndsWith("a"))
+{
+    Console.WriteLine("Kobieta");
+}
+else
+{
+    Console.WriteLine("Mężczyzna");
+}
+
+// nie umiem stworzyć wyniku w jednej linijce w zadaniu 1 i 3
